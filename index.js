d8ccb7f6ba2fe8417005fe34e8fd4871d0ec9961
g. nicholas d'andrea <nick@gnidan.org>
2017-05-20 13:29:21 -0400

    Use schema defaults to fill in object

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ var TruffleContractSchema = {
   // - Resolves as validated `contractObj`
   // - Rejects with list of errors from schema validator
   validate: function(contractObj) {
-    var ajv = new Ajv();
+    var ajv = new Ajv({ useDefaults: true });
     ajv.addSchema(abiSchema);
     ajv.addSchema(networkObjectSchema);
     ajv.addSchema(contractObjectSchema);
